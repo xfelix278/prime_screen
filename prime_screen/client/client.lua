0d522f8f73838e7ed0e2d55b7da7be08b61010d5
xfelix <60433189+xfelix278@users.noreply.github.com>
2023-08-04 17:31:41 +0200

    Update client.lua

--- prime_screen/client/client.lua
+++ prime_screen/client/client.lua
@@ -2,7 +2,7 @@ RegisterNetEvent("prime_screen:screenshot")
 AddEventHandler("prime_screen:screenshot", function(cl_webhook_url)
     exports["screenshot-basic"]:requestScreenshotUpload(cl_webhook_url, 'files[]', function(data)
         local img = json.decode(data)
-        if not img then print("Du hast vergessen die Storage-Webhook einzufügen") return end
+        if not img then print("Couldn't not load storage-webhook!") return end
         TriggerServerEvent("prime_screen:sendEmbed", img.attachments[1].url)
     end)
 end)
