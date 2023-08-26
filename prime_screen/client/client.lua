5c184cf83678fef33ff6f976cf3ae14aecb77a23
xfelix <60433189+xfelix278@users.noreply.github.com>
2023-08-26 13:07:13 +0200

    Update client.lua

--- prime_screen/client/client.lua
+++ prime_screen/client/client.lua
@@ -1,8 +1,9 @@
 RegisterNetEvent("prime_screen:screenshot")
 AddEventHandler("prime_screen:screenshot", function(cl_webhook_url)
-    exports["screenshot-basic"]:requestScreenshotUpload(cl_webhook_url, 'files[]', function(data)
-        local img = json.decode(data)
-        if not img then print("Couldn't not load storage-webhook!") return end
-        TriggerServerEvent("prime_screen:sendEmbed", img.attachments[1].url)
-    end)
+    if not GetInvokingResource() then
+        exports["screenshot-basic"]:requestScreenshotUpload(cl_webhook_url, 'files[]', function(data)
+            local img = json.decode(data)
+            TriggerServerEvent("prime_screen:sendEmbed", img.attachments[1].url)
+        end)
+    end
 end)
