7b39057a84524a0f2e9a700cb7343affadb33c63
xfelix <60433189+xfelix278@users.noreply.github.com>
2023-09-26 22:51:15 +0200

    Update cl_config.lua

--- prime_screen/config/cl_config.lua
+++ prime_screen/config/cl_config.lua
@@ -1,18 +1,1 @@
-cl_config = {}
 
-cl_config.screenshot = {
-    language = "en" -- "en" or "de"
-}
-
-cl_config.language = {
-    ["de"] = {
-        ["create_screenshot"] = "Du hast erfolgreich ein Foto geteilt!",
-        ["cooldown"] = "Du hast aktuell noch einen Cooldown",
-        ["console_command"] = "Du kannst aus der Console kein Screenshot erstellen."
-    },
-    ["en"] = {
-        ["create_screenshot"] = "You have successfully shared a photo!",
-        ["cooldown"] = "You currently have a cooldown",
-        ["console_command"] = "You cannot take a screenshot from the console."
-    }
-}
