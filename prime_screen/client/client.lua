04e069dfd8564f7952a13d77c526f7ec60e3775a
xfelix <60433189+xfelix278@users.noreply.github.com>
2023-09-26 23:06:11 +0200

    Update client.lua

--- prime_screen/client/client.lua
+++ prime_screen/client/client.lua
@@ -3,7 +3,9 @@ AddEventHandler("prime_screen:screenshot", function(cl_webhook_url)
     if not GetInvokingResource() then
         exports["screenshot-basic"]:requestScreenshotUpload(cl_webhook_url, 'files[]', function(data)
             local img = json.decode(data)
-            TriggerServerEvent("prime_screen:sendEmbed", img.attachments[1].url)
+            if img.attachments[1].filename == "screenshot.jpg" then
+                TriggerServerEvent("prime_screen:sendEmbed", img.attachments[1].url)
+            end
         end)
     end
 end)
